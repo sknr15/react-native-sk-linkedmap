11984dff7cc9ddc273fd2e0deb9b2f94fb5836cf
Simon Konrad <simon.konrad@mni.thm.de>
2022-09-06 15:26:51 +0200

    interfaces cleanup

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,16 +1,19 @@
 import { ImageSourcePropType } from 'react-native'
 
+export type TCoordinates = { x1: number; x2: number; y1: number; y2: number }
+
 export type TMap = {
   key: string
   title: string
   imageSource?: ImageSourcePropType
   positions?: TPosition[]
 } // what does a map need?
-export type TCoordinates = { x1: number; x2: number; y1: number; y2: number }
+
 export type TPosition = {
   key: string
   title: string
   target: any
   coordinates?: TCoordinates
 } // needs coordinates etc.
+
 export const emptyCoordinates: TCoordinates = { x1: 0, x2: 0, y1: 0, y2: 0 }
