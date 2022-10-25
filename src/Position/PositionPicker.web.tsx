1517a0dc295130482ac8db7c282f8ccc5487b3d5
Simon Konrad <simon.konrad@mni.thm.de>
2022-10-25 14:40:20 +0200

    web positionpicker

--- src/Position/PositionPicker.web.tsx
+++ src/Position/PositionPicker.web.tsx
@@ -3,6 +3,7 @@ import { Platform, ScrollView, TouchableOpacity, View } from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 import Image from 'react-native-scalable-image';
 import { emptyCoordinates, TCoordinates, TMap, TPosition } from '../interfaces';
+import { Rnd } from 'react-rnd';
 
 type ResizeDirection = 'top' | 'right' | 'bottom' | 'left' | 'topRight' | 'topLeft' | 'bottomRight' | 'bottomLeft';
 
@@ -16,6 +17,7 @@ type Props = {
 };
 
 export const PositionPicker = ({ height, map, onChange, position, testId, width }: Props) => {
+	const IS_WEB = Platform.OS === 'web';
 	const [containerSize, setContainerSize] = useState<{
 		height: number;
 		width: number;
@@ -68,6 +70,10 @@ export const PositionPicker = ({ height, map, onChange, position, testId, width 
 		let x = x1;
 		let y = y1;
 
+		if (imgZoomRef.current) {
+			x = x / imgZoomRef.current['scale'];
+			y = y / imgZoomRef.current['scale'];
+		}
 		if (x2 && y2) {
 			_coordinates = { x1: x, x2, y1: y, y2 };
 		} else {
@@ -170,25 +176,89 @@ export const PositionPicker = ({ height, map, onChange, position, testId, width 
 			const _height = ((y2 - y1) / 100) * sizeFactor.height;
 			const _width = ((x2 - x1) / 100) * sizeFactor.width;
 
+			const posX = (x1 / 100) * sizeFactor.width * (imgZoomRef.current ? imgZoomRef.current['scale'] : 1) + sizeFactor.x;
+			const posY = (y1 / 100) * sizeFactor.height * (imgZoomRef.current ? imgZoomRef.current['scale'] : 1) + sizeFactor.y;
+
+			if (isHorizontal) {
+				if (posX > 0 && x2 > 0 && y2 > 0 && scrollRef.current && !hasScrolled) {
+					let scrollTo = Math.max(posX + _width / 2 - containerSize.width / 2, 0);
+					scrollRef.current.scrollTo({ x: scrollTo, y: 0 });
+					setHasScrolled(true);
+				}
+			} else {
+				if (posY > 0 && x2 > 0 && y2 > 0 && scrollRef.current && !hasScrolled) {
+					let scrollTo = Math.max(posY + _height / 2 - containerSize.height / 2, 0);
+					scrollRef.current.scrollTo({ x: 0, y: scrollTo });
+					setHasScrolled(true);
+				}
+			}
+
 			return (
-				<View
-					key={`${testId}_map_position_${position.key}`}
-					testID={`${testId}_map_position_${position.key}`}
-					style={{
-						position: 'absolute',
-						left: (x1 / 100) * sizeFactor.width + sizeFactor.x,
-						top: (y1 / 100) * sizeFactor.height + sizeFactor.y,
+				<Rnd
+					default={{
+						x: posX,
+						y: posY,
 						height: Math.max(_height, 0),
-						width: Math.max(_width, 0),
-						borderWidth: x2 && y2 ? 2 : 1,
-						borderColor: 'red',
-						justifyContent: 'center',
-						alignItems: 'center',
-						zIndex: 10,
-						backgroundColor: '#ff000033'
+						width: Math.max(_width, 0)
 					}}
-					pointerEvents={'none'}
-				/>
+					position={{
+						x: posX,
+						y: posY
+					}}
+					size={{
+						height: Math.max(_height, 0),
+						width: Math.max(_width, 0)
+					}}
+					style={{
+						backgroundColor: '#ff000099',
+						borderColor: 'red',
+						borderWidth: 2,
+						alignItems: 'center',
+						justifyContent: 'center'
+					}}
+					onDragStart={() => setIsSingleTouch(true)}
+					onDrag={(e, d) => {
+						setIsSingleTouch(false);
+						if (d.deltaX || d.deltaY) {
+							let _coordinates = { ...newCoordinates };
+							const { x1, x2, y1, y2 } = _coordinates;
+							_coordinates = {
+								..._coordinates,
+								x1: x1 + (d.deltaX / sizeFactor.width) * 100,
+								x2: x2 + (d.deltaX / sizeFactor.width) * 100,
+								y1: y1 + (d.deltaY / sizeFactor.height) * 100,
+								y2: y2 + (d.deltaY / sizeFactor.height) * 100
+							};
+							setNewCoordinates({ ..._coordinates });
+						}
+					}}
+					onDragStop={() => {
+						setTimeout(() => {
+							setIsSingleTouch(true);
+						}, 500);
+					}}
+					onResizeStop={(e, direction, ref, delta, position) => {
+						let deltaX = (delta.width / sizeFactor.width) * 100;
+						let deltaY = (delta.height / sizeFactor.height) * 100;
+
+						_handleResize(direction, deltaX, deltaY);
+					}}
+					bounds={'parent'}
+					cancel="body"
+				>
+					{x2 === 0 && y2 === 0 && (
+						<View
+							style={{
+								position: 'absolute',
+								top: -2,
+								left: -2,
+								height: 4,
+								width: 4,
+								backgroundColor: 'red'
+							}}
+						></View>
+					)}
+				</Rnd>
 			);
 		}
 
@@ -197,39 +267,40 @@ export const PositionPicker = ({ height, map, onChange, position, testId, width 
 
 	const _renderMap = () => {
 		if (map && map.imageSource) {
+			const size = Math.max(height ?? containerSize.height, width ?? containerSize.width);
 			return (
-				<View
-					style={{
-						flex: 1,
-						alignSelf: 'center',
-						alignItems: 'center',
-						justifyContent: 'center',
-						backgroundColor: 'transparent'
-					}}
-					onTouchStart={(e) => {
-						setIsSingleTouch(true);
-					}}
-					onTouchMove={() => setIsSingleTouch(false)}
-					onTouchEnd={(e) => {
-						const { locationX, locationY } = e.nativeEvent;
-						if (isSingleTouch) {
-							_handleCoordinates((locationX / sizeFactor.width) * 100, (locationY / sizeFactor.height) * 100);
-						}
-					}}
-				>
-					<Image
-						testID={`${testId}_map_image`}
-						source={map.imageSource}
-						resizeMode="contain"
-						resizeMethod="scale"
-						height={height ?? containerSize.height}
-						width={width ?? containerSize.width}
-						onLayout={(e) => {
-							setSizeFactor(e.nativeEvent.layout);
+				<ScrollView ref={scrollRef} horizontal={isHorizontal}>
+					<TouchableOpacity
+						activeOpacity={1}
+						style={{
+							flex: 1,
+							alignSelf: 'center',
+							alignItems: 'center',
+							justifyContent: 'center',
+							backgroundColor: 'transparent'
 						}}
-					/>
-					{_renderPosition()}
-				</View>
+						onPressOut={(e) => {
+							const { locationX, locationY } = e.nativeEvent;
+							if (isSingleTouch) {
+								_handleCoordinates((locationX / sizeFactor.width) * 100, (locationY / sizeFactor.height) * 100);
+							}
+						}}
+					>
+						<Image
+							testID={`${testId}_map_image`}
+							source={map.imageSource}
+							resizeMode="contain"
+							resizeMethod="scale"
+							height={Math.max(size, containerSize.height)}
+							width={Math.max(size, containerSize.width)}
+							onLayout={(e) => {
+								setSizeFactor(e.nativeEvent.layout);
+								setHasScrolled(false);
+							}}
+						/>
+						{_renderPosition()}
+					</TouchableOpacity>
+				</ScrollView>
 			);
 		}
 	};
@@ -252,7 +323,7 @@ export const PositionPicker = ({ height, map, onChange, position, testId, width 
 					imageHeight={height ?? containerSize.height}
 					imageWidth={width ?? containerSize.width}
 					minScale={1}
-					maxScale={3}
+					maxScale={1}
 					enableDoubleClickZoom={false}
 				>
 					{_renderMap()}
